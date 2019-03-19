033e5b24070aaa0070137b16cf5d536ef8d455d1
Patrick Kim <pkim050@gmail.com>
2019-03-19 03:35:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,6 +1,6 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
-  "SELECT name, age FROM bears WHERE gender = 'female'"
+  "SELECT name, age FROM bears WHERE gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -10,7 +10,7 @@ end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
-  "SELECT name, age FROM bears WHERE alive = true ORDER BY age asc"
+  "SELECT name, age FROM bears WHERE alive = '1' ORDER BY age asc"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -25,12 +25,12 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
-  "SELECT COUNT(color) FROM bears WHERE color = 'prominent'"
+  "SELECT MAX(COUNT(color)) FROM bears GROUP BY color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
-  "SELECT count(temperaments) FROM bears WHERE temperament = 'goofy'"
+  "SELECT count(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
